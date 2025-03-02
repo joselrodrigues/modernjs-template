766a560865d4f86e2623bca8228a7338c94c8cb5
joselrodrigues <ing.joserodrigues@gmail.com>
2025-03-02 17:06:08 -0300

    feat: add prettier and configure with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { ignores: ['node_modules/', 'dist/', 'build/'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { settings: { react: { version: 'detect' } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ];
--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,15 +1,9 @@
-import { Helmet } from '@modern-js/runtime/head';
+import React from 'react';
+
 import './index.css';
 
 const Index = () => (
   <div className="container-box">
-    <Helmet>
-      <link
-        rel="icon"
-        type="image/x-icon"
-        href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
-      />
-    </Helmet>
     <main>
       <div className="title">
         Welcome to
